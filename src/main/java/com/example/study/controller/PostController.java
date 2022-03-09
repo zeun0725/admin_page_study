8ec6b931c40209fa29b8ec2ae7456822cb65bcf3
zeun0725 <zeun0725@naver.com>
2022-03-10 01:01:37 +0900

    post Method

---
+++ src/main/java/com/example/study/controller/PostController.java
@@ -0,0 +1,32 @@
+package com.example.study.controller;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+
+    // ==== POST 사용 이유 ====
+    // HTML <Form> 태그
+    // ajax 검색
+    // http post body -> data
+    // json, xml, multipart-form / text-plain
+
+    // @RequestMapping(method = RequestMethod.POST, path = "/postMethod")
+    @PostMapping(value = "/postMethod")
+    public SearchParam postMethod(@RequestBody SearchParam searchParam){
+
+       return searchParam;
+    }
+
+    @PutMapping("/putMethod")
+    public void put(){
+
+    }
+
+    @PatchMapping("/patchMethod")
+    public void patch(){
+
+    }
+}
