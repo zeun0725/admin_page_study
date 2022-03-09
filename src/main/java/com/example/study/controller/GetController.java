98a2a6cdacf106dce3540bb282f9fe8d984ed13f
zeun0725 <zeun0725@naver.com>
2022-03-10 00:45:15 +0900

    get Method

---
+++ src/main/java/com/example/study/controller/GetController.java
@@ -0,0 +1,35 @@
+package com.example.study.controller;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api") // localhost:8080/api
+public class GetController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/getMethod") // localhost:8080/api/getMethod
+    public String getRequest(){
+
+        return "Hi getMethod";
+    }
+
+    @GetMapping("/getParameter") // localhost:8080/api/getParameter?id=1234&password=abcd
+    public String getParameter(@RequestParam String id, @RequestParam(name = "password") String pwd){
+        String password = "bbbb";
+        System.out.println("id: "+id);
+        System.out.println("pwd: "+pwd);
+        return id+pwd;
+    }
+
+    // localhost:8080/api/getMultiParameter?account=abc&email=study@gmail.com&page=10
+    // 검색변수가 늘어나는 상황일 때
+    @GetMapping("/getMultiParameter")
+    public SearchParam getMultiParameter(SearchParam searchParam){
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+        // json 형식 통신 {"account": "", "email": "", "page": 0}
+        return searchParam;
+    }
+}
