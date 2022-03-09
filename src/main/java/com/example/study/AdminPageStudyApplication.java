02aaf3f463bd4024653e9d3315b70e7321284a15
zeun0725 <zeun0725@naver.com>
2022-03-09 23:57:22 +0900

    Initial commit

---
+++ src/main/java/com/example/study/AdminPageStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPageStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPageStudyApplication.class, args);
+	}
+
+}
