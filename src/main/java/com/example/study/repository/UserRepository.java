ae0c3c6611055c0a270ee6deeee879a0d5fb511f
zeun0725 <zeun0725@naver.com>
2022-03-10 22:20:25 +0900

    jpa

---
+++ src/main/java/com/example/study/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.study.repository;
+
+import com.example.study.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // 기본적인 CRUD 실행 가능
+}
