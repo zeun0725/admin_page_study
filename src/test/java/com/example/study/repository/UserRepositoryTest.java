c4ef9ce67641124242060745045d36bdaa8816c1
zeun0725 <zeun0725@naver.com>
2022-03-11 01:14:07 +0900

    jpa test code 2

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -2,10 +2,14 @@ package com.example.study.repository;
 
 import com.example.study.AdminPageStudyApplicationTests;
 import com.example.study.model.entity.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends AdminPageStudyApplicationTests {
 
@@ -28,15 +32,43 @@ public class UserRepositoryTest extends AdminPageStudyApplicationTests {
         System.out.println("new User: "+newUser);
     }
 
+    @Test
     public void read() {
+        Optional<User> user = userRepository.findById(2L); // long type => 2L
 
+        user.ifPresent(selectUser -> {
+            System.out.println("user :"+selectUser);
+            System.out.println("email :"+selectUser.getEmail());
+        });
     }
 
+    @Test
+    @Transactional
     public void update() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("PPPP");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
 
     }
 
+    @Test
+    @Transactional // 마지막에 roll back transaction 해서 실 데이터 영향 없음
     public void delete() {
+        Optional<User> user = userRepository.findById(3L);
+
+        Assertions.assertTrue(user.isPresent()); // true
+
+        user.ifPresent(selectUser -> userRepository.delete(selectUser));
+
+        Optional<User> deleteUser = userRepository.findById(3L);
+
+        Assertions.assertFalse(deleteUser.isPresent()); // false
 
     }
 }
