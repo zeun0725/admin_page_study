1b19c70243623ce20c5b415af7557b0bf0617e94
zeun0725 <zeun0725@naver.com>
2022-03-22 02:24:16 +0900

    repository test code

---
+++ src/test/java/com/example/study/repository/AdminUserRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.example.study.repository;
+
+import com.example.study.AdminPageStudyApplicationTests;
+import com.example.study.model.entity.AdminUser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class AdminUserRepositoryTest extends AdminPageStudyApplicationTests {
+
+    @Autowired
+    private AdminUserRepository adminUserRepository;
+
+    @Test
+    public void create() {
+        AdminUser adminUser = new AdminUser();
+        adminUser.setAccount("AdminUser01");
+        adminUser.setPassword("AdminUser01");
+        adminUser.setStatus("REGISTERED");
+        adminUser.setRole("SUPER");
+        adminUser.setCreatedAt(LocalDateTime.now());
+        adminUser.setCreatedBy("AdminServer");
+
+        AdminUser newAdminUser = adminUserRepository.save(adminUser);
+        Assertions.assertNotNull(newAdminUser);
+
+    }
+}
