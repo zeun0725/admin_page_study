417d1441c38dbd90171e89ce5b419c1597f3dbef
zeun0725 <zeun0725@naver.com>
2022-03-22 02:14:18 +0900

    repository test code

--- src/test/java/com/example/study/repository/OrderDetailRepositoryTest.java
+++ src/test/java/com/example/study/repository/OrderDetailRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class OrderDetailRepositoryTest extends AdminPageStudyApplicationTests {
@@ -17,12 +18,14 @@ public class OrderDetailRepositoryTest extends AdminPageStudyApplicationTests {
     public void create() {
         OrderDetail orderDetail = new OrderDetail();
 
-
-        // 어떤 사람?
-//        orderDetail.setUser(4L);
-
-        // 어떤 상품?
-//        orderDetail.setItemId(1L);
+        orderDetail.setStatus("WATITING");
+        orderDetail.setArrivalDate(LocalDateTime.now().plusDays(2));
+        orderDetail.setQuantity(1);
+        orderDetail.setTotalPrice(BigDecimal.valueOf(900000));
+        orderDetail.setCreatedAt(LocalDateTime.now());
+        orderDetail.setCreatedBy("AdminServer");
+        orderDetail.setOrderGroupId(1L); // 어떠한 장바구니에
+        orderDetail.setItemId(1L); // 어떠한 상품
 
         OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
         Assertions.assertNotNull(newOrderDetail);
