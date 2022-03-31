f52fab76e5610c9629a6f62cb85d54d3f9c3ed9e
zeun0725 <zeun0725@naver.com>
2022-03-31 18:32:09 +0900

    user api create

--- src/main/java/com/example/study/service/UserApiLogicService.java
+++ src/main/java/com/example/study/service/UserApiLogicService.java
@@ -1,12 +1,15 @@
 package com.example.study.service;
 
 import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.User;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.UserApiRequest;
 import com.example.study.model.network.response.UserApiResponse;
 import com.example.study.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UserApiLogicService implements CrudInterface<UserApiRequest, UserApiResponse> {
@@ -19,8 +22,25 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
     // 3. 생성된 데이터 -> UserApiResponse return
 
     @Override
-    public Header<UserApiResponse> create(UserApiRequest request) {
-        return null;
+    public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+
+        // 1. request data
+        UserApiRequest userApiRequest = request.getData();
+
+        // 2. user 생성
+        User user = User.builder()
+                .account(userApiRequest.getAccount())
+                .password(userApiRequest.getPassword())
+                .status("REGISTERED")
+                .phoneNumber(userApiRequest.getPhoneNumber())
+                .email(userApiRequest.getEmail())
+                .registeredAt(LocalDateTime.now())
+                .build();
+
+        User newUser = userRepository.save(user);
+
+        // 3. 생성된 데이터 -> UserApiResponse return
+        return response(newUser);
     }
 
     @Override
@@ -29,12 +49,29 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
     }
 
     @Override
-    public Header<UserApiResponse> update(UserApiRequest request) {
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
         return null;
     }
 
     @Override
     public Header delete(Long id) {
         return null;
+    }
+
+    private Header<UserApiResponse> response(User user) {
+        // user -> userApiResponse return
+        UserApiResponse userApiResponse = UserApiResponse.builder()
+                .id(user.getId())
+                .account(user.getAccount())
+                .password(user.getPassword()) // todo 암호화, 길이 리턴
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .status(user.getStatus())
+                .registeredAt(user.getRegisteredAt())
+                .unregisteredAt(user.getUnregisteredAt())
+                .build();
+
+        // Header + data return
+        return Header.OK(userApiResponse);
     }
 }
