c61e37b585d7b6dbd130e4451794c313c10f808c
zeun0725 <zeun0725@naver.com>
2022-04-06 18:23:47 +0900

    item api crud

--- src/main/java/com/example/study/controller/api/ItemApiController.java
+++ src/main/java/com/example/study/controller/api/ItemApiController.java
@@ -4,34 +4,40 @@ import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.ItemApiRequest;
 import com.example.study.model.network.response.ItemApiResponse;
+import com.example.study.service.ItemApiLogicService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/item")
 public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+
+    @Autowired
+    private ItemApiLogicService itemApiLogicService;
+
     @Override
     @PostMapping("") // /api/item
     public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
-        return null;
+        return itemApiLogicService.create(request);
     }
 
     @Override
     @GetMapping("{id}") // /api/item/{id}
     public Header<ItemApiResponse> read(@PathVariable Long id) {
-        return null;
+        return itemApiLogicService.read(id);
     }
 
     @Override
     @PutMapping("") // /api/item
     public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
-        return null;
+        return itemApiLogicService.update(request);
     }
 
     @Override
     @DeleteMapping("{id}") // /api/item/{id}
     public Header delete(@PathVariable Long id) {
-        return null;
+        return itemApiLogicService.delete(id);
     }
 }
