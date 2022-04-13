4b635c3296815161d1807a33d5d9a26e1342f0f2
zeun0725 <zeun0725@naver.com>
2022-04-13 11:59:32 +0900

    ordergroup api crud and enum add

--- src/main/java/com/example/study/controller/api/OrderGroupApiController.java
+++ src/main/java/com/example/study/controller/api/OrderGroupApiController.java
@@ -24,18 +24,18 @@ public class OrderGroupApiController implements CrudInterface<OrderGroupApiReque
     @Override
     @GetMapping("{id}")
     public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
-        return null;
+        return orderGroupApiLogicService.read(id);
     }
 
     @Override
     @PutMapping("")
     public Header<OrderGroupApiResponse> update(@RequestBody Header<OrderGroupApiRequest> request) {
-        return null;
+        return orderGroupApiLogicService.update(request);
     }
 
     @Override
     @DeleteMapping("{id}")
     public Header delete(@PathVariable Long id) {
-        return null;
+        return orderGroupApiLogicService.delete(id);
     }
 }
